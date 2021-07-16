9cb3b6702bdd61fc11193eb985c26d8f5b5547ba
heriswn <heri15002@mail.unpad.ac.id>
2021-07-16 21:33:24 +0700

    add db nonton

--- nonton/models.py
+++ nonton/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    time = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    url = models.URLField(default='youtube.com/c/txtdrustadz')
+    description = models.TextField()
+
+    def __str__(self):
+        return "{}.{}".format(self.id,self.title)
--- nonton/views.py
+++ nonton/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 
+from .models import Post
+
 def index(request):
+    posts = Post.objects.all()
     context = {
         'title': 'Nonton | txtdrustadz Channel',
         'heading': 'Kumpulan kajian dari para guru-guru kita',
         'subheading': 'txtdrustadz Channel hadir bersama mengatasi masalah dikeseharian kita',
+        'Posts': posts,
     }
     return render(request, 'nonton/index.html', context)
