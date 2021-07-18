e42598a125f8a472bf0c16ee50faf127054ddbf1
heriswn <heri15002@mail.unpad.ac.id>
2021-07-18 10:00:39 +0700

    form layouting with bootstrap

--- form/forms.py
+++ form/forms.py
@@ -2,5 +2,38 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
+    name = forms.CharField(
+        label='Name',
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Input your Name',
+            }
+        )
+    )
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Input your Password',
+            }
+        ))
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder':'Input your Password',
+            }
+        )
+    )
+    agree = forms.BooleanField(
+        label='Agree',
+        widget=forms.CheckboxInput(
+            attrs={
+                'class':'form-check-input',
+            }
+        )
+    )
